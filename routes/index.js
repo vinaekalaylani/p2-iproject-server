4ed7077bcc8d14b1c1546f70a8520788df03ac78
vinaekalaylani <vinaekalaylani@gmail.com>
2021-10-19 20:56:44 +0700

    finish register feature

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const router = require(`express`).Router()
+const users = require(`./users`)
 
 router.get('/', (req, res) => res.send(`HALOOOO`))
+router.use(`/users`, users)
 
 module.exports = router
