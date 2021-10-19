065c573d72b4084853b15e26e95b3ab71d0ddea4
Vina Eka Laylani <88227833+vinaekalaylani@users.noreply.github.com>
2021-10-19 23:46:35 +0700

    Revert "Edit status"

--- routes/users.js
+++
@@ -1,8 +0,0 @@
-const UsersController = require("../controllers/usersController")
-
-const router = require(`express`).Router()
-
-router.post(`/regis`, UsersController.register)
-router.post(`/login`, UsersController.login)
-
-module.exports = router
