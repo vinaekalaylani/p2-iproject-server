5c6f160d2653b51fd1deb54010bfd31459034025
vinaekalaylani <vinaekalaylani@gmail.com>
2021-10-21 11:10:22 +0700

    finish login oauth

--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,6 @@ const router = require(`express`).Router()
 
 router.post(`/regis`, UsersController.register)
 router.post(`/login`, UsersController.login)
+router.post(`/login-google`, UsersController.loginGoogle)
 
 module.exports = router
